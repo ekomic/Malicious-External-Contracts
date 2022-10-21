5ec17e41c22a7c889bb64c1283a61e93253b7a9b
Chris Ekom <ekomchris2016@gmail.com>
2022-10-21 18:43:24 +0100

    Create prevention.sol

---
+++ prevention.sol
@@ -0,0 +1,12 @@
+Prevention
+Make the address of the external contract public and also get your external contract verified so that all users can view the code
+
+Create a new contract, instead of typecasting an address into a contract inside the constructor. So instead of doing Helper(_helper) where you are typecasting _helper address into a contract which may or may not be the Helper contract, create an explicit new helper contract instance using new Helper().
+
+Example
+
+contract Good {
+    Helper public helper;
+    constructor() {
+        helper = new Helper();
+    }
